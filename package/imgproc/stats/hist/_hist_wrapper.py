4ec84cc3cca2509a90650fbfc0af21fb025feff0
Yi Yao <leaf_yao8621@hotmail.com>
2021-09-09 11:40:05 -0700

    Added documentation

--- package/imgproc/stats/hist/_hist_wrapper.py
+++ package/imgproc/stats/hist/_hist_wrapper.py
@@ -4,26 +4,52 @@ import matplotlib.pyplot
 import imgproc.stats.hist._hist
 
 class Hist:
+    """[summary]
+    """
     def __init__(self,
                  hist: numpy.ndarray,
                  histr: numpy.ndarray,
                  histg: numpy.ndarray,
                  histb: numpy.ndarray):
+        """[summary]
+
+        Args:
+            hist (numpy.ndarray): [description]
+            histr (numpy.ndarray): [description]
+            histg (numpy.ndarray): [description]
+            histb (numpy.ndarray): [description]
+        """
         self.hist = hist
         self.histr = histr
         self.histg = histg
         self.histb = histb
-    def __str__(self):
+    def __str__(self) -> str:
+        """[summary]
+
+        Returns:
+            str: [description]
+        """
         return f"Hist:\n{self.hist}\n" +\
                f"Histr:\n{self.histr}\n" +\
                f"Histg:\n{self.histg}\n" +\
                f"Histb:\n{self.histb}\n"
-    def __repr__(self):
+    def __repr__(self) -> str:
+        """[summary]
+
+        Returns:
+            str: [description]
+        """
         return f"Hist:\n{self.hist}\n" +\
                f"Histr:\n{self.histr}\n" +\
                f"Histg:\n{self.histg}\n" +\
                f"Histb:\n{self.histb}\n"
     def plot(self, fn: typing.Union[None, str]=None):
+        """[summary]
+
+        Args:
+            fn (typing.Union[None, str], optional): [description].
+            Defaults to None.
+        """
         matplotlib.pyplot.clf()
         matplotlib.pyplot.bar(numpy.arange(256), self.hist)
         if (fn):
